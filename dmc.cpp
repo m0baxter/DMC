7d05615f3f271a85ded1b8192dbae07f97c19806
Matthew Baxter <m0baxter@gmail.com>
2018-05-14 17:00:44 -0400

    Fixed formatting to make it look better

--- dmc.cpp
+++ dmc.cpp
@@ -8,20 +8,19 @@
 double V( std::array<double, 1> &x ) {
 
    return 0.5 * x[0] * x[0];
-
 }
 
 int main() {
 
-   const int d = 1;                   //Spatial dimension
-   const int s = 1;                   //Number of particles
-   const int dp = d*s;                //Effective dimension.
-   int N0 = 1000;                     //Intial numberof points.
+   const int d = 1;                  //Spatial dimension
+   const int s = 1;                  //Number of particles
+   const int dp = d*s;               //Effective dimension.
+   int N0 = 1000;                    //Intial numberof points.
    const int maxN = 2000;            //Maximum number of points.
-   int nb = 200;                        //Number of bins for wave function histogram.
+   int nb = 200;                     //Number of bins for wave function histogram.
    double xmin = -5;                 //Lower bound of grid
    double xmax = 5;                  //Upper bound of grid
-   double timeStep = 0.001;           //Time step.
+   double timeStep = 0.001;          //Time step.
    double relaxTime = 50;            //Time to wait for convergence.
    double alpha = 5.0;               //Update parameter for energy.
 
